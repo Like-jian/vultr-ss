db4504a82dc9125751203bd31353030465da9f02
Like-jian <31382788+Like-jian@users.noreply.github.com>
2018-12-13 11:11:06 +0800

    Update vultr-ss.sh

--- vultr-ss.sh
+++ vultr-ss.sh
@@ -13,3 +13,7 @@ firewall-cmd --zone=public --add-port=2018/tcp --permanent
 firewall-cmd --reload
 firewall-cmd --list-all
 
+echo '# /bin/bash
+ssserver -c /etc/shadowsocks.json -d start' > /etc/rc.d/init.d/shadowsocks
+ls -s /etc/rc.d/init.d/shadowsocks /etc/rc.d/rc3.d/S60shadowsocks
+echo 'shadowscocks加入开机自启'
