e6a29c380b527841a9c4663da0b5764a012821b5
Like-jian <31382788+Like-jian@users.noreply.github.com>
2019-02-19 15:57:36 +0800

    Update vultr-ss.sh

--- vultr-ss.sh
+++ vultr-ss.sh
@@ -13,7 +13,24 @@ firewall-cmd --zone=public --add-port=2018/tcp --permanent
 firewall-cmd --reload
 firewall-cmd --list-all
 
-echo '# /bin/bash
-ssserver -c /etc/shadowsocks.json -d start' > /etc/rc.d/init.d/shadowsocks
-ln -s /etc/rc.d/init.d/shadowsocks /etc/rc.d/rc3.d/S60shadowsocks
+
+echo '[Unit]
+Description=shadowsocks
+After=auditd.service systemd-user-sessions.service time-sync.target network.target
+
+[Service]
+Type=forking
+ExecStart=/usr/bin/ssserver -c /etc/shadowsocks.json -d start
+ExecReload=/usr/bin/ssserver -d reload
+ExecStop=/usr/bin/ssserver -d stop
+
+[Install]
+WantedBy=multi-user.target
+' > /usr/lib/systemd/system/ssserver.service
+
+systemctl enable ssserver.service
 echo 'shadowscocks加入开机自启'
+timedatectl set-timezone Asia/Shanghai
+echo '
+1 3 * * * root reboot' >> /etc/crontab
+echo '每天凌晨3点重启服务器'
