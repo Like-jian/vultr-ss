0d1c4c0a52ae0ec0872d1a7034df95666f56d31b
Like-jian <31382788+Like-jian@users.noreply.github.com>
2019-02-19 16:08:35 +0800

    Update vultr-ss.sh

--- vultr-ss.sh
+++ vultr-ss.sh
@@ -30,6 +30,8 @@ WantedBy=multi-user.target
 
 systemctl enable ssserver.service
 echo 'shadowscocks加入开机自启'
+cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+hwclock -w
 timedatectl set-timezone Asia/Shanghai
 echo '
 1 3 * * * root reboot' >> /etc/crontab
