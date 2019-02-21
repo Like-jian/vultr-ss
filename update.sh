deb92bfc888606c7ff389af9753ede1d0a1204ba
Like-jian <31382788+Like-jian@users.noreply.github.com>
2019-02-21 08:56:49 +0800

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,22 @@
+echo '[Unit]
+Description=shadowsocks
+After=auditd.service systemd-user-sessions.service time-sync.target network.target
+[Service]
+Type=forking
+ExecStart=/usr/bin/ssserver -c /etc/shadowsocks.json -d start
+ExecReload=/usr/bin/ssserver -d reload
+ExecStop=/usr/bin/ssserver -d stop
+[Install]
+WantedBy=multi-user.target
+' > /usr/lib/systemd/system/ssserver.service
+
+systemctl enable ssserver.service
+echo 'shadowscocks加入开机自启'
+cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+hwclock -w
+timedatectl set-timezone Asia/Shanghai
+echo '
+1 3 * * * root reboot' >> /etc/crontab
+echo '每天凌晨3点重启服务器
+当前时间是：'
+date
