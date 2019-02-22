8d6412058e586dfd0d5d34015648733a66472d2d
Like-jian <31382788+Like-jian@users.noreply.github.com>
2019-02-22 16:19:00 +0800

    Create restartSS.sh

---
+++ restartSS.sh
@@ -0,0 +1,1 @@
+pkill -9 ssserver && echo -e ' {\n    "server_port":30001,\n    "password":"jmt123!@#",\n    "timeout":300,\n    "method":"aes-256-cfb"\n}' > /etc/shadowsocks.json && ssserver -c /etc/shadowsocks.json -d start
