6480d715a6bedd34a276be91b9f914676a909009
Like-jian <31382788+Like-jian@users.noreply.github.com>
2019-02-23 15:25:58 +0800

    Update vultr-ss.sh

--- vultr-ss.sh
+++ vultr-ss.sh
@@ -4,13 +4,13 @@ yum -y install python-pip
 yum -y install ntpdate
 pip install shadowsocks
 echo ' { 
-    "server_port":2018,
+    "server_port":30001,
     "password":"jmt123!@#",
     "timeout":300,
     "method":"aes-256-cfb"
 }' > /etc/shadowsocks.json
 ssserver -c /etc/shadowsocks.json -d start
-firewall-cmd --zone=public --add-port=2018/tcp --permanent
+firewall-cmd --zone=public --add-port=30001/tcp --permanent
 firewall-cmd --reload
 firewall-cmd --list-all
 
