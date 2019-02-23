9f667086d5f6b3c1f9ce80aa79c9d479d525ffca
Like-jian <31382788+Like-jian@users.noreply.github.com>
2019-02-23 15:24:38 +0800

    Update restartSS.sh

--- restartSS.sh
+++ restartSS.sh
@@ -1,1 +1,7 @@
-pkill -9 ssserver && echo -e ' {\n    "server_port":30001,\n    "password":"jmt123!@#",\n    "timeout":300,\n    "method":"aes-256-cfb"\n}' > /etc/shadowsocks.json && ssserver -c /etc/shadowsocks.json -d start
+pkill -9 ssserver 
+echo -e ' {\n    "server_port":30001,\n    "password":"jmt123!@#",\n    "timeout":300,\n    "method":"aes-256-cfb"\n}' > /etc/shadowsocks.json 
+ssserver -c /etc/shadowsocks.json -d start
+firewall-cmd --zone=public --add-port=30001/tcp --permanent
+firewall-cmd --reload
+firewall-cmd --list-all
+ 
