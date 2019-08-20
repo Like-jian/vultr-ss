c2ad2d50e87400bbbcef33522737d725e6c7272f
Like-jian <31382788+Like-jian@users.noreply.github.com>
2019-08-20 15:29:55 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -17,8 +17,6 @@ iptables -A INPUT -p tcp --dport 30001 -j ACCEPT
 # 显示当前时间
 hwclock -w
 timedatectl set-timezone Asia/Shanghai
-echo '
-#1 3 * * * root reboot' >> /etc/crontab
-echo '每天凌晨3点重启服务器
+
 当前时间是：'
 date
