0fe5315b99ccc6f6604e36519b5cd2e5bcfe7a6b
Like-jian <31382788+Like-jian@users.noreply.github.com>
2019-09-20 15:06:31 +0800

    Create v2ray_creat.sh

---
+++ v2ray_creat.sh
@@ -0,0 +1,28 @@
+wget https://install.direct/go.sh 
+. go.sh 
+echo '{
+  "inbounds": [
+    {
+      "port": 3001, // 服务器监听端口
+      "protocol": "vmess",    // 主传入协议
+      "settings": {
+        "clients": [
+          {
+            "id": "b831381d-6324-4d53-ad4f-8cda48b30811",  // 用户 ID，客户端与服务器必须相同
+            "alterId": 64
+          }
+        ]
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",  // 主传出协议
+      "settings": {}
+    }
+  ]
+}
+
+' 
+systemctl start v2ray 
+systemctl stop firewalld
